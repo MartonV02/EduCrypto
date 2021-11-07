fe323963c99e53bd0160ee775f31305d562c05fb
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-11-07 12:29:18 +0100

    User_Crypto table

--- Database/SQL_for_eduCripto.sql
+++ Database/SQL_for_eduCripto.sql
@@ -3,3 +3,15 @@
 	COLLATE utf8_hungarian_ci;
 
 use eduCrypto;
+
+
+
+CREATE table user_crypto (
+  id int,
+  walletNumber varchar (34) NOT NULL,
+  cryptoType varchar (20) NOT NULL,
+  cryptoValue float NOT NULL,
+  CONSTRAINT pk_user_crypto PRIMARY KEY (id),
+  CONSTRAINT fk_walletNumerInFree FOREIGN KEY (walletNumber) REFERENCES user_finance(walletNumber),
+  CONSTRAINT fk_walletNumerInGroup FOREIGN KEY (walletNumber) REFERENCES users_handling_for_groups(walletNumber),
+);
