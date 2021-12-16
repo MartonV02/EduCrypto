2ced6f35a626419d138fd02fd5da596a181e945c
MartonV02 <varga.marton@students.jedlik.eu>
2021-12-16 09:06:03 +0100

    Added app and started implementation

---
+++ Application/Common/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
+
+namespace Application.Common
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public string ConnectionString { get; set; }
+#if DEBUG
+        = "Server=localhost;Database=WorkSheet;Uid=root;Pwd=;";
+#endif
+        public ApplicationDbContext() { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+            var extension = options.FindExtension<MySqlOptionsExtension>();
+            this.ConnectionString = extension.ConnectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConnectionString);
+        }
+
+        public static ApplicationDbContext AppDbContext { get; set; }
+    }
+}
---
+++ Application/Common/GenericApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Common
+{
+    public class GenericApplicationService<T>
+    {
+        public GenericApplicationService()
+        {
+            
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            //var result = ;
+            //return result;
+        }
+    }
+}
