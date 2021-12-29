f651aff6825bd999464465813b8a8f17eb78d405
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-12-29 16:59:00 +0100

    Delete unsued usings from Application (so far), close #46

--- Application/UserHandling/UserHandling.cs
+++ Application/UserHandling/UserHandling.cs
@@ -1,9 +1,5 @@
 ﻿using Application.Images;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.UserHandling
 {
