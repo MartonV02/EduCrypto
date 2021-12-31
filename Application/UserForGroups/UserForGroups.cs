6cf6d2c92e4dc23bf0d8cfe8f614b8d4b8e88cfe
Márton, Varga <varga.marton@students.jedlik.eu>
2021-12-31 12:44:33 +0100

    Created the model and the interface for app service to UserForGroups

---
+++ Application/UserForGroups/UserForGroups.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.UserForGroups
+{
+    public class UserForGroups : IUserForGroups
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [Required]
+        public int groupId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string accesLevel { get; set; }
+
+        [Required]
+        [MaxLength(34)]
+        public string groupWalletNumber { get; set; }
+
+        [Required]
+        [Range(0, 999999999999999.99)]
+        public decimal money { get; set; }
+    }
+}
