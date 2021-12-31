c70626f666054ab604c2fb1ddd0d86b3f5e3b830
Márton, Varga <varga.marton@students.jedlik.eu>
2021-12-31 12:06:03 +0100

    Created the interface for UserCrypto appservice

---
+++ Application/UserCrypto/Interfaces/IUserCryptoAppService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EntityClass = Application.UserCrypto.UserCrypto;
+
+namespace Application.UserCrypto.Interfaces
+{
+    internal interface IUserCryptoAppService
+    {
+        public IEnumerable<EntityClass> GetAll();
+        public IEnumerable<EntityClass> GetById(int id);
+        public IEnumerable<EntityClass> Create();
+        public IEnumerable<EntityClass> Update();
+        public IEnumerable<EntityClass> Delete();
+    }
+}
