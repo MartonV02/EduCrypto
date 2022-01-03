ca6d690f64539bb7b1e598c672fa3542367473df
Klenovszky_Áron_103 <aron.klenovszky@gmail.com>
2022-01-03 09:33:49 +0100

    extended Group Interface and model - closes #47 , closes #15

---
+++ Application/Group/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Group
+{
+    class Group: IGroup
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public decimal startBudget { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime finishDate { get; set; }
+        public bool isFinished { get; set; } = true;
+    }
+}
