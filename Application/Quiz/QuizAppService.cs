4987725dddb77d9af0aa6af162c7c9ce1735c681
Klenovszky_Áron_103 <aron.klenovszky@gmail.com>
2022-01-10 09:12:28 +0100

    QuizModel and App Service

---
+++ Application/Quiz/QuizAppService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Quiz
+{
+    class QuizAppService
+    {
+        public List<QuizModel> Questions { get; set; }
+
+        public QuizAppService()
+        {
+            StreamReader sr = new StreamReader("question.txt");
+            while (!sr.EndOfStream)
+            {
+                Questions.Add( new QuizModel(sr.ReadLine()));
+            }
+        }
+
+        public QuizModel GetQuestion(int  xp)
+        {
+            return Questions[xp];
+        }
+
+        public bool CheckQuestion(byte answer, int xp)
+        {
+            return Questions[xp].IsRight(answer);
+        }
+    }
+}
