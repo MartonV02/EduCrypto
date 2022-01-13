a5a9091574c53a2b1a555b2ebc5c8f5166b0fa72
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2022-01-13 10:02:07 +0100

    Some succes

---
+++ Application/Migrations/20220113085736_EduCrypro_v1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Migrations
+{
+    public partial class EduCrypro_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
