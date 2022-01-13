85d04fd179744eb41e730603395db8df6f044684
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2022-01-13 12:47:46 +0100

    Some more progress with DB
    V3 is nearly great, but some foreign key is missing

--- Application/Common/ApplicationDbContext.cs
+++ Application/Common/ApplicationDbContext.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
 using System;
+using Application.UserHandling;
 
 namespace Application.Common
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<Application.UserHandling.UserHandling> UserHandlings { get; set; }
+        public DbSet<Application.CryptoCurrencies.CryptoCurrency> CryptoCurrencies { get; set; }
+        public DbSet<Application.Group.Group> Groups { get; set; }
+        public DbSet<Application.UserCrypto.UserCrypto> UserCryptos { get; set; }
+        public DbSet<Application.UserFinance.UserFinance> UserFinances { get; set; }
+        public DbSet<Application.UserForGroups.UserForGroups> UserForGroups { get; set; }
+        public DbSet<Application.UserTradeHistory.UserTradeHistory> UserTradeHistories { get; set; }
         public string ConnectionString { get; set; }
 #if DEBUG
         = "Server=localhost;Database=WorkSheet;Uid=root;Pwd=;";
--- Application/Migrations/20220113114229_EduCrypro_v3.Designer.cs
+++ Application/Migrations/20220113114229_EduCrypro_v3.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Application.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Application.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220113114229_EduCrypro_v3")]
+    partial class EduCrypro_v3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
