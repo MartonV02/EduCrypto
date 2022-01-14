c254e70485d03a148e9f713dd5de2c71d32ac851
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2022-01-14 20:48:56 +0100

    Add some stuff so we don't have to wait for ng serve at every backend start

--- EduCrypto/Startup.cs
+++ EduCrypto/Startup.cs
@@ -91,7 +91,8 @@ namespace EduCrypto
                 //Comment for Me(Kleno)
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    //spa.UseAngularCliServer(npmScript: "start");
                 }
             });
         }
