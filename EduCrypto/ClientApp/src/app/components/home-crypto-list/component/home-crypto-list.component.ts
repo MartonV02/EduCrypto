9c820be83242d9ab8499e1fe89104728edcff64e
Márton, Varga <varga.marton@students.jedlik.eu>
2022-01-18 23:02:53 +0100

    Try to ad cryptocurrencies to the site

---
+++ EduCrypto/ClientApp/src/app/components/home-crypto-list/component/home-crypto-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-home-crypto-list',
+  templateUrl: './home-crypto-list.component.html',
+  styleUrls: ['./home-crypto-list.component.scss']
+})
+export class HomeCryptoListComponent {
+
+}
--- EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
+++ EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
@@ -1,1 +1,2 @@
-//https://codesource.io/create-a-cryptocurrency-app-with-angular/
+//https://github.com/sbuntz/InstantCrypto/blob/main/assets/js/script.js
+//https://coursetro.com/posts/code/91/Angular-CryptoCurrency-Tutorial---Display-Exchange-Data-with-an-API
