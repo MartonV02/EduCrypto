918ebca42df0cfce1db9ddf0ef552e907a811f17
Márton, Varga <varga.marton@students.jedlik.eu>
2022-01-18 23:18:58 +0100

    Source links in service

--- EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
+++ EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
@@ -1,2 +1,3 @@
 //https://github.com/sbuntz/InstantCrypto/blob/main/assets/js/script.js
 //https://coursetro.com/posts/code/91/Angular-CryptoCurrency-Tutorial---Display-Exchange-Data-with-an-API
+//Using CoinMarket API
