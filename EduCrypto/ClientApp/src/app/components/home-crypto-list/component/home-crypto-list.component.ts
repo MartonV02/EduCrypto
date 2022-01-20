0b7df0db02973cea952cd2ae32af826d3e0e2d7e
Márton, Varga <varga.marton@students.jedlik.eu>
2022-01-20 22:40:53 +0100

    Find a way to import cryptos

--- EduCrypto/ClientApp/src/app/components/home-crypto-list/component/home-crypto-list.component.ts
+++ EduCrypto/ClientApp/src/app/components/home-crypto-list/component/home-crypto-list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-home-crypto-list',
@@ -7,14 +9,27 @@ import { Component } from "@angular/core";
 })
 export class HomeCryptoListComponent
 {
-  private _queryURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&ids=${coinID}`;
+  //private _queryURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&ids=${coinID}`; - can be an alternative
   //https://www.youtube.com/watch?v=2xOgkCT7MOQ
-  constructor()
+  //https://www.youtube.com/watch?v=292A8yq2U2A - option A 10k request free - every 5 min in a month
+  //https://algotrading101.com/learn/coingecko-api-guide/
+
+  private _http;
+  private _queryURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+
+  constructor(private http: HttpClient)
   {
+    this._http = http;
   }
 
-  getListOfCryptos(): any
+  public getListOfCryptos(): Observable<>
   {
-
+    this._http.get( this._queryURL,
+      {
+        headers:
+        {
+          'X-CMC_PRO_API_KEY': 'a32bf73f-f24a-4484-8d59-220e19acd17d',
+        }
+      })
   }
 }
--- EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
+++ EduCrypto/ClientApp/src/app/components/home-crypto-list/service/import-crypto-currencies.service.ts
@@ -1,4 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
 //https://github.com/sbuntz/InstantCrypto/blob/main/assets/js/script.js
 //https://coursetro.com/posts/code/91/Angular-CryptoCurrency-Tutorial---Display-Exchange-Data-with-an-API
 //https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100
 //Using CoinGecko API
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeCryptoListService {
+  private _http;
+
+  constructor(private http: HttpClient) {
+    this._http = http;
+  }
+
+  public getListOfCryptos(): Observable<> {
+    this._http.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      {
+        headers:
+        {
+          'X-CMC_PRO_API_KEY': 'a32bf73f-f24a-4484-8d59-220e19acd17d',
+        }
+      })
+  }
+}
