5a5a330a37bd7b653f60ed32e40215c6a347cddf
Klenovszky_Áron_203 <aron.klenovszky@gmail.com>
2022-02-11 10:20:26 +0100

    Rewrite Models and Interfaces

--- Application/UserCrypto/Interfaces/IUserCrypto.cs
+++ Application/UserCrypto/Interfaces/IUserCrypto.cs
@@ -1,5 +1,4 @@
 ﻿using Application.Common;
-using Application.CryptoCurrencies;
 using Application.UserForGroups;
 using Application.UserHandling;
 
@@ -8,7 +7,7 @@ namespace Application.UserCrypto
     public interface IUserCrypto : IIdentity
     {
         public UserHandlingModel userHandlingModel { get; set; }
-        public CryptoCurrencyModel cryptoCurrency { get; set; }
+        public string cryptoSymbol { get; set; }
         public decimal cryptoValue { get; set; }
         public UserForGroupsModel? userForGroupsModel { get; set; }
         public bool isFavourite { get; set; }
