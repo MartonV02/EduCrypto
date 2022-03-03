0bf938efd7bd9c3e04584bb8a26b7bdc649415c6
szucscsombor <szucs.abel.csombor@students.jedlik.eu>
2022-03-03 08:23:43 +0100

    profile + theming imrpovements

--- EduCrypto/ClientApp/src/app/app.component.ts
+++ EduCrypto/ClientApp/src/app/app.component.ts
@@ -4,22 +4,22 @@ import { Component, Inject, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
 
   public isDarkRecaptcha: boolean;
 
   switchMode(isDarkMode: boolean) {
     const baseClass = isDarkMode ? 'theme-dark' : 'theme-light';
     this.renderer.setAttribute(this.document.body, 'class', baseClass);
-    if (isDarkMode)
-    {
-      this.isDarkRecaptcha =true;
-    }
-    else {
+    if (isDarkMode) {
+      this.isDarkRecaptcha = true;
+    } else {
       this.isDarkRecaptcha = false;
     }
   }
