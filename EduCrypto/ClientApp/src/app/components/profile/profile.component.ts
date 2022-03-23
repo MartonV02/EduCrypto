13aeea23f140e31d14122a29c982b8d9e4b082a6
szucscsombor <szucs.abel.csombor@students.jedlik.eu>
2022-03-23 16:54:36 +0100

    Profile page component Fixes #109

--- EduCrypto/ClientApp/src/app/components/profile/profile.component.ts
+++ EduCrypto/ClientApp/src/app/components/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LegendPosition, Color, colorSets } from '@swimlane/ngx-charts';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -8,46 +7,8 @@ import { LegendPosition, Color, colorSets } from '@swimlane/ngx-charts';
 
 export class ProfileComponent implements OnInit {
 
+  ngOnInit(): void {}
+
   constructor() {}
 
-  public Cryptos = [
-    {
-      name: 'Bitcoin',
-      value: '12',
-    },
-    {
-      name: 'Ethereum',
-      value: '35',
-    },
-    {
-      name: 'DogeCoin',
-      value: '54',
-    },
-    {
-      name: 'XRP',
-      value: '110',
-    },
-    {
-      name: 'LiteCoin',
-      value: '87',
-    },
-  ];
-
-  // public view: [number, number] = [400, 200];
-  public legendTitle: string = 'Cryptos';
-  public legendPosition: LegendPosition = LegendPosition.Below; //right or below;
-  public legend: boolean = true;
-  customColors = 
-  [
-    { name: "Bitcoin", value: '#F46197' },
-    { name: "Ethereum", value: '#3F88C5' },
-    { name: "DogeCoin", value: '#44BBA4' },
-    { name: "XRP", value: '#FFD23F' },
-    { name: "LiteCoin", value: '#3C91E6' },
-  ]
-  // public scheme: string | Color = "valami" 
-
-
-  ngOnInit(): void {
-  }
 }
