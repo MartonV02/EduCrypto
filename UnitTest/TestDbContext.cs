5a9e6d28761d208832a9d97fc86d9c6f175aa7fc
Klenovszky_Áron_103 <aron.klenovszky@gmail.com>
2022-03-28 09:41:02 +0200

    UserHandling Test, Modify Missing

---
+++ UnitTest/TestDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common;
+using Application.Group;
+using Application.UserForGroups;
+using Application.UserHandling;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class TestDbContext: ApplicationDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("educrypto" + Guid.NewGuid().ToString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public static TestDbContext GenerateTestDbContext()
+        {
+            TestDbContext context = new();
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
