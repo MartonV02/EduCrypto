b8debdd1c221e99ef6b3c096c4f60d1d75a54233
Kelovszky Áron <aron.klenovszky@gmail.com>
2022-03-31 09:39:54 +0200

    LeaderBoard
    Need collection export, and authorization

--- EduCrypto/Controllers/UserForGroupsController.cs
+++ EduCrypto/Controllers/UserForGroupsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Common;
 using Application.Common.Auth;
 using Application.Group;
+using Application.UserCrypto;
 using Application.UserForGroups;
 using Application.UserHandling;
 using Microsoft.AspNetCore.Authorization;
@@ -20,6 +21,7 @@ namespace EduCrypto.Controllers
         private readonly UserForGroupsAppService userForGroupsAppService;
         private readonly GroupAppService groupAppService;
         private readonly UserHandlingAppService userHandlingAppService;
+        private readonly UserCryptoAppService userCryptoAppService;
 
         public UserForGroupsController(ApplicationDbContext dbContext, IConfiguration config)
         {
@@ -29,6 +31,7 @@ namespace EduCrypto.Controllers
             userHandlingAppService = new UserHandlingAppService(dbContext);
             groupAppService = new GroupAppService(dbContext);
             userForGroupsAppService = new UserForGroupsAppService(dbContext);
+            userCryptoAppService = new UserCryptoAppService(dbContext);
             this.config = config;
         }
 
@@ -81,6 +84,16 @@ namespace EduCrypto.Controllers
                 if (!userForGroupsAppService.IsMember(groupId, AuthenticationExtension.GetUserIdFromToken(config, token)))
                     return Forbid();
                 return Ok(userForGroupsAppService.GetByGroupId(groupId));
+            });
+        }
+
+        [HttpGet("leaderBoard/{groupId}")]
+        [AllowAnonymous]
+        public ActionResult GetLeaderBoardByGroupId(int groupId)
+        {
+            return this.Run(() =>
+            {
+                return Ok(userForGroupsAppService.GetLeaderBordByGroupId(groupId, userHandlingAppService, userCryptoAppService));
             });
         }
 
